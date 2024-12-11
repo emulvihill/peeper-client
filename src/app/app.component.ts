1d2c412e7b6de7decde3e6eb30f04475cdfed361
Eric Mulvihill <eric.mulvihill@gmail.com>
2024-12-11 12:49:57 -0500

    separate comparison from creation of VideoSnaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { AfterViewChecked, Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {VideoSnapService} from "../service/video-snap.service";
-import {firstValueFrom} from "rxjs";
+import {firstValueFrom, Observable} from "rxjs";
+import {VideoSnap} from "../models/graphql-models";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
-export class AppComponent implements AfterViewChecked {
-  title = 'Peeper';
+export class AppComponent implements OnInit {
+  title = "Peeper";
 
   storage: ImageBitmap[] = []; // Use this array as our database
 
   stream: MediaStream | null = null;
-  captureInterval: ReturnType<typeof setTimeout>|undefined;
+  captureInterval: ReturnType<typeof setTimeout> | undefined;
   startBtn: HTMLButtonElement | undefined;
   stopBtn: HTMLButtonElement | undefined;
   hideVid: HTMLInputElement | undefined;
@@ -26,13 +27,13 @@ export class AppComponent implements AfterViewChecked {
   constructor(private videoSnapService: VideoSnapService) {
   }
 
-  ngAfterViewChecked() {
+  ngOnInit() {
     this.startBtn = document.querySelector("button#start")! as HTMLButtonElement;
     this.stopBtn = document.querySelector("button#stop")! as HTMLButtonElement;
     this.hideVid = document.querySelector("input#hideVid")! as HTMLInputElement;
     this.intervalSec = document.querySelector("input#interval")! as HTMLInputElement;
     this.videoElem = document.querySelector("video")! as HTMLVideoElement;
-    this.imageCountSpan = document.querySelector('span#image_count')! as HTMLSpanElement;
+    this.imageCountSpan = document.querySelector("span#image_count")! as HTMLSpanElement;
     this.imagesDiv = document.querySelector("div#images")! as HTMLDivElement;
   }
 
@@ -42,8 +43,8 @@ export class AppComponent implements AfterViewChecked {
     this.videoElem.hidden = this.hideVid.checked;
   }
 
-  async uploadData(result: string) {
-    await firstValueFrom(this.videoSnapService.createVideoSnap("1", result));
+  uploadData(result: string): Observable<VideoSnap> {
+    return this.videoSnapService.createVideoSnap("1", result);
   }
 
   async startClick() {
@@ -51,19 +52,29 @@ export class AppComponent implements AfterViewChecked {
 
     this.startBtn.disabled = true;
 
-    this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    this.stream = await navigator.mediaDevices.getUserMedia({video: true});
     this.videoElem.onplaying = () =>
       console.log("video playing this.stream:", this.videoElem!.srcObject);
     this.videoElem.srcObject = this.stream;
 
 
     // (data check on the interval value) * that value is in seconds  * frame timestamps are in microseconds
-    const intervalSecValue: number = parseInt(this.intervalSec.value);
-    const interval = (intervalSecValue >= 1 ? intervalSecValue : 1) * 1000;
 
     clearInterval(this.captureInterval);
 
-    this.captureInterval = setInterval(async () => {
+    const interval = this.getTimoutInterval();
+    this.captureInterval = setTimeout(this.createSnapshot(), interval);
+
+    this.stopBtn.disabled = false;
+  }
+
+  private getTimoutInterval() {
+    const intervalSecValue: number = parseInt(this.intervalSec!.value);
+    return (intervalSecValue >= 1 ? intervalSecValue : 1) * 1000;
+  }
+
+  private createSnapshot() {
+    return async () => {
       const canvas = new OffscreenCanvas(1, 1); // needs an initial size
       const ctx = canvas.getContext("2d")!;
 
@@ -73,7 +84,7 @@ export class AppComponent implements AfterViewChecked {
       canvas.width = this.videoElem.videoWidth;
 
       const maxDimension = 128;
-      const reduction = Math.max(canvas.width, canvas.height)/maxDimension;
+      const reduction = Math.max(canvas.width, canvas.height) / maxDimension;
       const dw = Math.floor(canvas.width / reduction);
       const dh = Math.floor(canvas.height / reduction);
       canvas.width = dw;
@@ -82,16 +93,23 @@ export class AppComponent implements AfterViewChecked {
       canvas.convertToBlob().then(blob => {
         const reader = new FileReader();
         reader.onloadend = async () => {
-          reader.result && await this.uploadData(reader.result as string);
+          if (reader.result) {
+
+            const snap = await firstValueFrom(this.uploadData(reader.result.toString()));
+            console.info(`Image uploaded, ${snap.id} ${snap.date}`);
+          } else {
+            console.error("No result bitmap to upload!");
+          }
           const bitmap: ImageBitmap = canvas.transferToImageBitmap();
           this.storage.push(bitmap);
-        }
+        };
         reader.readAsDataURL(blob);
       });
       this.imageCountSpan.innerText = (parseInt(this.imageCountSpan.innerText) + 1).toString();
-    }, interval);
 
-    this.stopBtn.disabled = false;
+      const interval = this.getTimoutInterval();
+      this.captureInterval = setTimeout(this.createSnapshot(), interval);
+    };
   }
 
   async stopClick() {
--- src/service/video-snap.service.ts
+++ src/service/video-snap.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {VideoSnap} from "../models/graphql-models";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class VideoSnapService {
-  private apiUrl = 'http://localhost:5000/graphql'; // Replace with your actual GraphQL API URL
+  private apiUrl = "http://localhost:5000/graphql"; // Replace with your actual GraphQL API URL
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  createVideoSnap(feedId: string, data: string): Observable<any> {
+  createVideoSnap(feedId: string, data: string): Observable<VideoSnap> {
     const query = `
       mutation CreateVideoSnap($feedId: ID!, $data: String!) {
         createVideoSnap(input: {feedId: $feedId, data: $data}) {
@@ -20,6 +22,9 @@ export class VideoSnapService {
       }
     `;
 
-    return this.http.post(this.apiUrl, { query, variables: { feedId, data } });
+    return this.http.post(this.apiUrl, {
+      query,
+      variables: {feedId, data},
+    }).pipe(map((res: any) => res.data.createVideoSnap)) as Observable<VideoSnap>;
   }
 }
